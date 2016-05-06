aa4ec01ab7edf6c4bfd3c54f7daa0fdfcbfe2cb7
Marina Rozova <marinar578@gmail.com>
2016-05-06 14:51:04 -0400

    added the animations I want to index

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,17 @@
 "use strict";
 (function(){
-    $('.main-container').fadeIn(8000);
+
+    $('#index-logo .logo').fadeIn('slow');
+    $('#index-logo .logo h1').delay(400).slideDown('slow');
+    $('#index-logo .logo #logo-desc').delay(1000).fadeIn('slow');
+
+
+    $('#index-logo .navigation').slideDown('slow')
+
+    $('.main-container').fadeIn('slow')
+    for(var i = 1; i < 7; i++){
+        $('#project-' + i).delay(i*100).fadeIn('slow');
+    }
+
+
 })();
