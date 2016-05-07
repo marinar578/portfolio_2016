9d3e17aa0679e722dbdf8ec2462974f2a69b5c42
Marina Rozova <marinar578@gmail.com>
2016-05-07 17:55:58 -0400

    add resume

--- public/js/script.js
+++ public/js/script.js
@@ -8,22 +8,9 @@
     $('#index-logo .logo #logo-desc').delay(700).fadeTo('slow', 1);
     $('#index-logo .navigation').delay(1000).fadeTo('slow', 1);
 
-    $('.main-container').fadeTo('slow', 1);
+    $('.main-container').delay(200).fadeTo('slow', 1);
     for(var i = 1; i < 7; i++){
         $('#project-' + i).delay(200).fadeTo('slow', 1);
     }
-
-    /* Menu */
-      // var menu = $('.navigation li');
-      // menu.hover(function(){
-      //   $(this).children('ul').show();
-      //   $('.main-container').fadeTo('fast', .5)
-      // }, function(){
-      //   $(this).children('ul').hide();
-      //   $('.main-container').fadeTo('fast', 1)
-      // });
-
-
-
 
 })();
